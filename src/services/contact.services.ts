d55c6d3ceb1bce6dc5852efb0ad39ddcbeb809cd
Melissa Gasque <melissagasque.com>
2023-11-27 12:05:45 -0300

    feat:postContact com id

--- src/services/contact.services.ts
+++ src/services/contact.services.ts
@@ -1,10 +1,10 @@
 import { DeepPartial, Repository } from "typeorm"
 import { AppDataSource } from "../data-source"
 import { AppError } from "../errors/App.errors"
-import { ContactsInterface, ContactsInterfaceNoClient, CreateContactsInterface } from "../interfaces/Contacts.interface"
+import { ContactsInterfaceNoClient, CreateContactsInterface } from "../interfaces/Contacts.interface"
 import { Client, Contact } from "../entities"
-import { readContactSchema } from "../schemas/contacts.schema"
 import { ClientsInterface } from "../interfaces/Clients.interface"
+import { contactSchemaNoClient } from "../schemas/contacts.schema"
 
 
 
@@ -13,12 +13,13 @@ export const createContactsService = async (payload: CreateContactsInterface, cl
     const contactsRepo: Repository<ContactsInterfaceNoClient> = AppDataSource.getRepository(Contact)
     const clientRepo: Repository<ClientsInterface> = AppDataSource.getRepository(Client)
     const client = await clientRepo.findOneBy({id: clientId})
-    const contact: ContactsInterfaceNoClient = contactsRepo.create(payload)
-    await contactsRepo.save({...contact, client: client})
-    return contact  //O ID NÃO ESTÁ SENDO ENVIADO
+    const contactBody = contactsRepo.create(payload)
+    const contactConected = await contactsRepo.save({...contactBody, client: client})
+    const contact = contactSchemaNoClient.parse(contactConected)
+    return contact
 }
 
-//Listagem dos contatos
+//Listagem dos contatos -> Tem acesso apenas dono da conta
 export const readContactsService = async (idToken: string): Promise<ContactsInterfaceNoClient[]> => {
     const contactsRepo: Repository<Contact> = AppDataSource.getRepository(Contact)
 
@@ -36,7 +37,7 @@ export const readContactsService = async (idToken: string): Promise<ContactsInte
 }
 
 
-// Atualiza contatos -> Apenas adminstradores ou dono da conta
+// Atualiza contatos -> Apenas dono da conta
 export const updateContactsService = async (
     payload: DeepPartial<Contact>,
     contactId: string,
@@ -58,12 +59,11 @@ export const updateContactsService = async (
 
     const updatedContact = await contactsRepo.save({ ...contact, ...payload })
 
-    console.log(updatedContact)
     return updatedContact
    
   }
 
-// Deleta a conta do contatos
+// Deleta contatos -> Apenas dono da conta
 export const deleteContacsService = async (contactId: string, idToken: string): Promise<void> => {
     const contactsRepo = AppDataSource.getRepository(Contact)
     const customerContacts =await contactsRepo.find({
